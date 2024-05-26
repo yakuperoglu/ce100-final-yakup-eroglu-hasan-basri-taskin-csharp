960e553e6483e4f14575943bf140093f718138a1
Hasan Basri Taşkın <41389642+WatchDogs2TR@users.noreply.github.com>
2024-05-26 12:47:04 +0300

    App function are added

--- TaskschedulerApp/Program.cs
+++ TaskschedulerApp/Program.cs
@@ -1,9 +1,19 @@
-﻿internal class Program {
-  private static void Main(string[] args) {
-
+﻿/**
+ * @class Program
+ * @brief The main entry point for the application.
+ */
+internal class Program
+{
+    /**
+     * @brief The main method of the application.
+     * @param args The command line arguments.
+     */
+    private static void Main(string[] args)
+    {
+        string pathFileUsers = "users.bin";
+        string pathFileTasks = "tasks.bin";
+        string pathFileCategories = "categories.bin";
         var TaskschedulerLibrary = new TaskschedulerLibrary.Taskscheduler();
-
-        TaskschedulerLibrary.mainmenu();
-        
+        TaskschedulerLibrary.MainMenu(pathFileUsers, pathFileTasks, pathFileCategories);
     }
 }
